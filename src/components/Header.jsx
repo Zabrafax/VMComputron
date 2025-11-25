73d43d17f632cb8f94a27abf5aba7be616b17929
Aliaksandr Semutsenka <aliaksandr.semutsenka@student.tuke.sk>
2025-11-25 20:02:55 +0100

    Basic layout added

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header style={{
+      width: `100%`,
+      height: `43px`,
+
+      backgroundColor: 'blue',
+    }}>
+      Header
+    </header>
+  );
+}
+
+export default Header;
