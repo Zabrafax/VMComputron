83b1bcd9c2adf746afb2e701e99f62f4558c41de
Vladyslav Krykun <mr.laggermpc.890@gmail.com>
2025-12-09 16:10:51 +0100

    colors refactoring

--- frontend/src/components/memoryWindow/Registers.jsx
+++ frontend/src/components/memoryWindow/Registers.jsx
@@ -36,7 +36,7 @@ function Registers({ className }) {
             >
               <path
                 d="M14 10V12.6667C14 13.0203 13.8595 13.3594 13.6095 13.6095C13.3594 13.8595 13.0203 14 12.6667 14H3.33333C2.97971 14 2.64057 13.8595 2.39052 13.6095C2.14048 13.3594 2 13.0203 2 12.6667V10M11.3333 5.33333L8 2M8 2L4.66667 5.33333M8 2V10"
-                stroke="#F5F5F5" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round"
+                strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round"
               />
             </svg>
             <p className={styles.File__button__title}>
@@ -49,7 +49,7 @@ function Registers({ className }) {
             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path
                 d="M14 10V12.6667C14 13.0203 13.8595 13.3594 13.6095 13.6095C13.3594 13.8595 13.0203 14 12.6667 14H3.33333C2.97971 14 2.64057 13.8595 2.39052 13.6095C2.14048 13.3594 2 13.0203 2 12.6667V10M4.66667 6.66667L8 10M8 10L11.3333 6.66667M8 10V2"
-                stroke="#F5F5F5" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round"
+                strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round"
               />
             </svg>
             <p className={styles.File__button__title}>
